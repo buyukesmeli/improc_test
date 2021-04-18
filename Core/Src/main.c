9d740fc812c3012ad68e94805f5cde2a00457e2a
Huseyin Buyukesmeli <buyukesmeli@gmail.com>
2021-04-18 21:44:05 +0300

    Added grayscale conversions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -32,6 +32,7 @@
 #include "sdram.h"
 #include "image.h"
 #include "ImageProcessing.h"
+#include "image_conversion.h"
 #include "lcd/image_show.h"
 /* USER CODE END Includes */
 
@@ -47,9 +48,9 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-volatile uint32_t *DWT_CTRL = (uint32_t *)0xE0001000;
-volatile uint32_t *DWT_CYCCNT = (uint32_t *)0xE0001004;
-volatile uint32_t *DEMCR = (uint32_t *) 0xE000EDFC;
+volatile uint32_t *DWT_CTRL = (uint32_t*) 0xE0001000;
+volatile uint32_t *DWT_CYCCNT = (uint32_t*) 0xE0001004;
+volatile uint32_t *DEMCR = (uint32_t*) 0xE000EDFC;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -57,7 +58,8 @@ volatile uint32_t *DEMCR = (uint32_t *) 0xE000EDFC;
 /* USER CODE BEGIN PV */
 uint32_t CoreClock;
 uint32_t CycleCount;
-float ExecutionTime1,ExecutionTime2,ExecutionTime3,ExecutionTime4,ExecutionTime5,ExecutionTime6,ExecutionTime7;
+float ExecutionTime1, ExecutionTime2, ExecutionTime3, ExecutionTime4,
+		ExecutionTime5, ExecutionTime6, ExecutionTime7;
 
 uint32_t i;
 uint32_t data[1000];
@@ -71,155 +73,156 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void StartTiming(void){
-*DEMCR = *DEMCR | 0x01000000;
-DWT->LAR = 0xC5ACCE55;
-*DWT_CTRL = *DWT_CTRL | 1;
-*DWT_CYCCNT = 0;
+void StartTiming(void) {
+	*DEMCR = *DEMCR | 0x01000000;
+	DWT->LAR = 0xC5ACCE55;
+	*DWT_CTRL = *DWT_CTRL | 1;
+	*DWT_CYCCNT = 0;
 }
-uint32_t StopTiming(void){
-return *DWT_CYCCNT;
+uint32_t StopTiming(void) {
+	return *DWT_CYCCNT;
 }
-float CalculateTime_ms(uint32_t cycle, uint32_t frequency)
-{
-return (float) 1000*cycle/frequency;
+float CalculateTime_ms(uint32_t cycle, uint32_t frequency) {
+	return (float) 1000 * cycle / frequency;
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* Enable I-Cache---------------------------------------------------------*/
-  SCB_EnableICache();
+	/* Enable I-Cache---------------------------------------------------------*/
+	SCB_EnableICache();
 
-  /* Enable D-Cache---------------------------------------------------------*/
-  SCB_EnableDCache();
+	/* Enable D-Cache---------------------------------------------------------*/
+	SCB_EnableDCache();
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_FMC_Init();
-  MX_USART6_UART_Init();
-  MX_DMA2D_Init();
-  MX_LTDC_Init();
-  /* USER CODE BEGIN 2 */
-  SDRAM_Initialization_sequence(0x0603);
-  /* USER CODE END 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_FMC_Init();
+	MX_USART6_UART_Init();
+	MX_DMA2D_Init();
+	MX_LTDC_Init();
+	/* USER CODE BEGIN 2 */
+	SDRAM_Initialization_sequence(0x0603);
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-    Image img;
-    ConfigureImage(&img, (uint8_t *) IMAGE_ADDRESS_1, 128, 128, 1, IMAGE_FORMAT_RGB888, IMAGE_DATA_TYPE_UINT8);
-    uint32_t img_bpp = 1;
-    memset((uint8_t *) IMAGE_ADDRESS_1,0,img.Width*img.Height*img_bpp);
-    HAL_StatusTypeDef res = HAL_UART_Receive(&huart6, img.pData, img.Width*img.Height*img_bpp,50000);
-    if(res != HAL_OK){
-  	  while(1);
-    }
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	Image img, gray_img;
+	ConfigureImage(&img, (uint8_t*) IMAGE_ADDRESS_1, 128, 128, 1,
+			IMAGE_FORMAT_RGB, IMAGE_DATA_TYPE_UINT8);
 
-    LTDC_LayerCfgTypeDef img_layer;
-    img_layer.FBStartAdress = LCD_FRAME_BUFFER;
-    img_layer.ImageWidth = 480;
-    img_layer.ImageHeight= 272;
-    img_layer.WindowX0 = 0;
-    img_layer.WindowY0 = 0;
+	memset((uint8_t*) IMAGE_ADDRESS_1, 0, img.Size);
+	HAL_StatusTypeDef res = HAL_UART_Receive(&huart6, img.pData, img.Size,
+			50000);
+	if (res != HAL_OK) {
+		while (1)
+			;
+	}
+	ConfigureImage(&gray_img, (uint8_t*) IMAGE_ADDRESS_2, 128, 128, 1,
+			IMAGE_FORMAT_MONO, IMAGE_DATA_TYPE_UINT8);
 
-    imshow_gray8(&img,&hdma2d,&img_layer);
 
-    while (1)
-    {
-    /* USER CODE END WHILE */
+	rgb_to_gray(&img, &gray_img);
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+	LTDC_LayerCfgTypeDef img_layer;
+	img_layer.FBStartAdress = LCD_FRAME_BUFFER;
+	img_layer.ImageWidth = 480;
+	img_layer.ImageHeight = 272;
+	img_layer.WindowX0 = 0;
+	img_layer.WindowY0 = 0;
+
+	imshow_gray8(&gray_img, &hdma2d, &img_layer);
+
+	while (1) {
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = { 0 };
 
-  /** Configure LSE Drive Capability
-  */
-  HAL_PWR_EnableBkUpAccess();
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 432;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/** Configure LSE Drive Capability
+	 */
+	HAL_PWR_EnableBkUpAccess();
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 25;
+	RCC_OscInitStruct.PLL.PLLN = 432;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 9;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+	/** Activate the Over-Drive mode
+	 */
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC|RCC_PERIPHCLK_USART6;
-  PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
-  PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
-  PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
-  PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV8;
-  PeriphClkInitStruct.PLLSAIDivQ = 1;
-  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_4;
-  PeriphClkInitStruct.Usart6ClockSelection = RCC_USART6CLKSOURCE_PCLK2;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+		Error_Handler();
+	}
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC
+			| RCC_PERIPHCLK_USART6;
+	PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
+	PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
+	PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
+	PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV8;
+	PeriphClkInitStruct.PLLSAIDivQ = 1;
+	PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_4;
+	PeriphClkInitStruct.Usart6ClockSelection = RCC_USART6CLKSOURCE_PCLK2;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -227,15 +230,14 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
